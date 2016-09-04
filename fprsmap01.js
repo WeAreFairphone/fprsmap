f1585928ea554088e4fcc18592eb6f8f3a27c7f0
Roboe <roboe@virgulilla.com>
2016-09-04 14:51:48 +0200

    Switch on SSL for map tiles

--- fprsmap01.js
+++ fprsmap01.js
@@ -7,8 +7,8 @@ var myIcon = L.icon({
 
 // Map is initialized
 var fprsmap = L.map('mapid').setView([49.8158683, 6.1296751], 4);
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPLv3</a>',
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPLv3</a>',
   maxZoom: 18,
 }).addTo(fprsmap);
 
