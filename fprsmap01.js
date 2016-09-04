2ef386e062cd11d80a36c5a9a7dd2f3f0d069e25
newluck77 <newluck77@gmail.com>
2016-09-04 14:22:21 +0200

    New branch for layerGroup implementation

--- fprsmap01.js
+++ fprsmap01.js
@@ -26,7 +26,7 @@
   function initMap() {
     map = L.map('mapid').setView([49.8158683, 6.1296751], 4);
     var mapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPLv3</a>',
+      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPLv3</a>',
       maxZoom: 18,
     });
     mapLayer.addTo(map);
@@ -35,17 +35,27 @@
   /* Main */
   initMap();
 
+  //Create a Layer
+  var groupslayer = L.layerGroup();
+
+  // Make a key value pair for controls
+  var overlayMaps = { "Fairphoners groups": groupslayer };
+
   fetchJSON('communities.json')
     .then(function(json) {
       // Add a marker per Local Fairphone Community
       json.list.forEach(function(group) {
-        L.marker(group.lat_lng, { icon: CUSTOM_MARKER, riseOnHover: true })
-          .addTo(map)
+        var marker = L.marker(group.lat_lng, { icon: CUSTOM_MARKER, riseOnHover: true })
           .bindPopup(
             '<a href="' + FORUM_THREAD_URL + '/' + group.post_nr + '" target="_blank">' + group.location + '</a>',
             { offset: new L.Point(0, -25) }
           );
-      });
+        groupslayer.addLayer(marker);
+      })
+    })
+    .then(function() {
+      L.control.layers(overlayMaps)
+        .addTo(map);
     });
 
 }(this));
