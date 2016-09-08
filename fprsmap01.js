15436423617f4431422b7a0760463d06a1159501
newluck77 <newluck77@users.noreply.github.com>
2016-09-09 00:38:33 +0200

    Layer controls visible by default, checkboxes

--- fprsmap01.js
+++ fprsmap01.js
@@ -11,7 +11,7 @@
 
   /* Constants */
   // Local Fairphone Communities forum thread
-  var FORUM_THREAD_URL = 'https://forum.fairphone.com/t/pencil2-local-fairphoners-address-book-fairphone-communities/3815';
+  var FORUM_THREAD_URL = 'https://forum.fairphone.com/t/pencil2-local-fairphoners-address-book-fairphone-communities/3815/';
 
   var CUSTOM_MARKER = L.icon({
     iconUrl: 'FairphoneMarker.png',
@@ -20,42 +20,54 @@
   });
 
   /* Variables (state) */
-  var map;
+  var map,
+      mapLayer,
+      groupslayer = L.layerGroup(),
+      tmobileshops = L.layerGroup(),
+      baseMaps,
+      overlayMaps;
 
   /* Functions */
-  function initMap() {
-    map = L.map('mapid').setView([49.8158683, 6.1296751], 4);
-    var mapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  function initMap(defaultlayer) {
+    map = L.map('mapid', {
+    center: [49.8158683, 6.1296751],
+    zoom: 4,
+    layers: [defaultlayer]
+    })
+    mapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPLv3</a>',
       maxZoom: 18,
     });
     mapLayer.addTo(map);
   }
 
+  function initControls(){
+    // Make a key value pair for controls
+    baseMaps = {
+
+    };
+    overlayMaps = { "Fairphoners groups": groupslayer,
+                        "T-Mobile Shops": tmobileshops
+    };
+    L.control.layers(baseMaps, overlayMaps, {collapsed:false})
+      .addTo(map);
+  }
+
   /* Main */
-  initMap();
+  initMap(groupslayer);
+  initControls();
 
-  //Create a Layer
-  var groupslayer = L.layerGroup();
 
-  // Make a key value pair for controls
-  var overlayMaps = { "Fairphoners groups": groupslayer };
-
-  fetchJSON('communities.json')
+  fetchJSON('https://newluck77.github.io/fprsmap/communities.json')
     .then(function(json) {
       // Add a marker per Local Fairphone Community
       json.list.forEach(function(group) {
         var marker = L.marker(group.lat_lng, { icon: CUSTOM_MARKER, riseOnHover: true })
           .bindPopup(
-            '<a href="' + FORUM_THREAD_URL + '/' + group.post_nr + '" target="_blank">' + group.location + '</a>',
+            '<a href="' + FORUM_THREAD_URL + group.post_nr + '" target="_blank">' + group.location + '</a>',
             { offset: new L.Point(0, -25) }
           );
         groupslayer.addLayer(marker);
       })
-    })
-    .then(function() {
-      L.control.layers(overlayMaps)
-        .addTo(map);
     });
-
 }(this));
