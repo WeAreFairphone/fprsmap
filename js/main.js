de8a936be2ed5276fda4d394269182a587aefba6
StefanBrand <StefanBrand@users.noreply.github.com>
2017-01-10 23:19:43 +0100

    Let attribution links open in new tab

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
 
   function initMap(defaultOverlays) {
     var baseLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a> | &copy; <a href="https://github.com/WeAreFairphone/fprsmap">WeAreFairphone</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3</a>)',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a> | &copy; <a href="https://github.com/WeAreFairphone/fprsmap" target="_blank">WeAreFairphone</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>)',
       maxZoom: 18,
     });
 
