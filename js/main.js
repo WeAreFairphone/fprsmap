b4e82754d78f6af5f4dade6145586e839583f38e
StefanBrand <StefanBrand@users.noreply.github.com>
2017-05-16 21:48:54 +0200

    Set default zoom level to 2. Fix #58

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@
 
     map = L.map('mapid', {
       center: [49.8158683, 6.1296751],
-      zoom: 4,
+      zoom: 2,
       minZoom: 2,
       layers: getInitialLayers(overlaysData, defaultOverlays, [baseLayer, cluster]),
       worldCopyJump: true,
