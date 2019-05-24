b09a1738d70bb93706576ad40c8ee82f05596d8e
Vitor Sexto Bernardes <vitorsb@gmail.com>
2019-05-24 17:25:27 +0100

    Add absolute paths for scripts
    Add absolute paths for scripts when downloading data files, so the
    scripts can be run manually from anywhere.

--- scripts/fetch-events.py
+++ scripts/fetch-events.py
@@ -3,5 +3,5 @@ import json, os.path, requests
 response = requests.get('https://forum.fairphone.com/agenda.json')
 
 if response.status_code == requests.codes.ok :
-    with open(os.path.dirname(__file__) + "/../data/events.json","w") as file:
+    with open(os.path.dirname(os.path.abspath(__file__)) + "/../data/events.json","w") as file:
         json.dump(response.json()['topic_list']['topics'], file, indent=2)
