d5aa8f4af13f5a20ec58609b9d8b9ede7cc629dd
Ingo-FP-Angel <35199584+Ingo-FP-Angel@users.noreply.github.com>
2021-08-01 14:16:06 +0000

    Deploying to gh-pages from @ WeAreFairphone/fprsmap@2f60ab72fa3b77fadca7f7f034892a34b2def955 🚀

--- scripts/fetch-events.py
+++ scripts/fetch-events.py
@@ -1,7 +1,9 @@
-import json, os.path, requests
+import json, os.path, requests, sys
 
 response = requests.get('https://forum.fairphone.com/agenda.json')
 
 if response.status_code == requests.codes.ok :
     with open(os.path.dirname(os.path.abspath(__file__)) + "/../data/events.json","w") as file:
         json.dump(response.json()['topic_list']['topics'], file, indent=2)
+else :
+    sys.exit(1)
