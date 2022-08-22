59c626052bf3db30fbe15810fa725c5c74d630f3
Ingo-FP-Angel <35199584+Ingo-FP-Angel@users.noreply.github.com>
2022-08-22 14:47:25 +0000

    Deploying to gh-pages from @ WeAreFairphone/fprsmap@63fbdc9d388c3b5633fadf39330ecd8cfd65cb11 🚀

--- js/main.js
+++ js/main.js
@@ -1,29 +1,39 @@
-(function(global) {
+(function (global) {
   'use strict';
 
   /* Utils */
   function fetchJSON(url) {
     return fetch(url)
-      .then(function(response) {
-        return response.json();
-      });
+        .then(function (response) {
+          return response.json();
+        });
   }
 
   function getQueries() {
-    if(!location.search) return {};
+    if (!location.search) return {};
 
     return location.search
-      .replace('?', '')
-      .split('&')
-      .reduce(function(queries, current) {
-        var splitted = current.split('=');
-        queries[splitted[0]] = splitted[1];
-        return queries;
-      }, {});
+        .replace('?', '')
+        .split('&')
+        .reduce(function (queries, current) {
+          var splitted = current.split('=');
+          queries[splitted[0]] = splitted[1];
+          return queries;
+        }, {});
   }
 
   /* Constants */
   var COMMUNITY_DOMAIN = 'fairphone.community'
+  var TOPIC_URL = 'https://forum.fairphone.com/t/'
+
+  var MARKERICONS = ["blue", "brown", "green", "grey", "orange", "pink", "red"]
+      .reduce(function (markericons, color) {
+        markericons[color] = L.icon({
+          iconUrl: 'resources/FairphoneMarker_' + color + '.png',
+          iconAnchor: [15.9, 49],
+        });
+        return markericons;
+      }, {});
 
   var EXCLUDED_LAYERS = [];
 
@@ -43,8 +53,12 @@
       title: "Fairphone Angels",
       overlay: L.featureGroup.subGroup(cluster),
     },
+    events: {
+      title: "Meetups & Events",
+      overlay: L.featureGroup.subGroup(cluster),
+    },
   }
-  var activeLayers = Object.keys(overlaysData).filter(function(key){
+  var activeLayers = Object.keys(overlaysData).filter(function (key) {
     return !EXCLUDED_LAYERS.includes(key);
   });
   var currentMapCenter = DEFAULTMAPCENTER;
@@ -55,18 +69,18 @@
   /* Functions */
   function getAllOverlays(overlaysData) {
     return Object.keys(overlaysData)
-      .reduce(function(overlays, currentOverlay){
-        overlays[overlaysData[currentOverlay].title] = overlaysData[currentOverlay].overlay;
-        return overlays;
-      }, {});
+        .reduce(function (overlays, currentOverlay) {
+          overlays[overlaysData[currentOverlay].title] = overlaysData[currentOverlay].overlay;
+          return overlays;
+        }, {});
   }
 
   function titleToKey(title) {
     return Object.keys(overlaysData)
-      .filter(function(key){
-        return (overlaysData[key].title == title)
-      })
-      .toString();
+        .filter(function (key) {
+          return (overlaysData[key].title == title)
+        })
+        .toString();
   }
 
   function isEmbedded() {
@@ -75,27 +89,27 @@
     return false;
   }
 
-  function isValidOverlayQueries(overlaysData,defaultOverlays){
-    return defaultOverlays.reduce(function(validQuery,currentQuery){
+  function isValidOverlayQueries(overlaysData, defaultOverlays) {
+    return defaultOverlays.reduce(function (validQuery, currentQuery) {
       return validQuery || Object.keys(overlaysData).includes(currentQuery);
     }, false);
   }
 
   function getInitialLayers(overlays, permanentLayers) {
-    return overlays.reduce(function(layers, currentOverlay) {
-          layers.push(overlaysData[currentOverlay].overlay);
-        return layers;
-      }, permanentLayers || []);
+    return overlays.reduce(function (layers, currentOverlay) {
+      layers.push(overlaysData[currentOverlay].overlay);
+      return layers;
+    }, permanentLayers || []);
   }
 
   function getEffectiveOverlays(overlaysData, defaultOverlays) {
     return Object.keys(overlaysData)
-      .filter(function(currentOverlay) {
-        if ((!defaultOverlays || !isValidOverlayQueries(overlaysData,defaultOverlays)) && EXCLUDED_LAYERS.includes(currentOverlay)) return false;
-        if (!defaultOverlays || !isValidOverlayQueries(overlaysData,defaultOverlays)) return true;
+        .filter(function (currentOverlay) {
+          if ((!defaultOverlays || !isValidOverlayQueries(overlaysData, defaultOverlays)) && EXCLUDED_LAYERS.includes(currentOverlay)) return false;
+          if (!defaultOverlays || !isValidOverlayQueries(overlaysData, defaultOverlays)) return true;
 
-        return defaultOverlays.indexOf(currentOverlay) !== -1;
-      });
+          return defaultOverlays.indexOf(currentOverlay) !== -1;
+        });
   }
 
   function initMap(initialMapCenter, initialZoomLevel, defaultOverlays) {
@@ -129,25 +143,26 @@
   function addPopupWithEmbedCode() {
     updateEmbedTextareaContentAndBrowserUrl();
     var embedPopupContent = 'Embed code:<br>' +
-      '<textarea autofocus cols="35" id="embed-textarea" readonly rows="3" wrap="off">' + embedTextareaContent + '</textarea>';
+        '<textarea autofocus cols="35" id="embed-textarea" readonly rows="3" wrap="off">' + embedTextareaContent + '</textarea>';
     L.popup({className: 'embed-popup'})
-    .setLatLng(map.getCenter())
-    .setContent(embedPopupContent)
-    .openOn(map);
+        .setLatLng(map.getCenter())
+        .setContent(embedPopupContent)
+        .openOn(map);
   }
 
   function updateEmbedTextareaContentAndBrowserUrl() {
     var queryParams = '?center=' + currentMapCenter[0] + ',' + currentMapCenter[1] + '&zoom=' + currentZoomLevel + '&show=' + activeLayers.toString();
     embedTextareaContent = '<iframe src="https://map.fairphone.community/' + queryParams + '" width="100%" height="400" allowfullscreen="true" frameborder="0">\n' +
-    '<p><a href="https://map.fairphone.community/' + queryParams + '" target="_blank">See the Fairphone Community Map!</a></p>\n' +
-    '</iframe>';
+        '<p><a href="https://map.fairphone.community/' + queryParams + '" target="_blank">See the Fairphone Community Map!</a></p>\n' +
+        '</iframe>';
     try {
       if (window.history.replaceState) {
         window.history.replaceState({}, null, queryParams);
       }
       document.getElementById('embed-textarea').value = embedTextareaContent;
-    } catch(e) {
-    };
+    } catch (e) {
+    }
+    ;
   }
 
   function initControls() {
@@ -157,14 +172,14 @@
     layerControls.addTo(map);
 
     // Add embed Control
-    L.easyButton('<img src="resources/embed-icon.png">', function(btn,map){
+    L.easyButton('<img src="resources/embed-icon.png">', function (btn, map) {
       addPopupWithEmbedCode();
-    },'Embed the map!').addTo(map);
+    }, 'Embed the map!').addTo(map);
   }
 
   function getInitialMapCenter() {
     var mapcenter = queryParams.center; // all queries after "center="
-    if(!mapcenter) {
+    if (!mapcenter) {
       return DEFAULTMAPCENTER;
     }
 
@@ -197,19 +212,19 @@
   }
 
   function onMovestart(e) {
-    if(!layerControls.collapsed) {
+    if (!layerControls.collapsed) {
       layerControls.collapse();
     }
   }
 
   function onMousedown(e) {
-    if(!map.scrollWheelZoom.enabled()) {
+    if (!map.scrollWheelZoom.enabled()) {
       map.scrollWheelZoom.enable();
     }
   }
 
   function onMouseout(e) {
-    if(map.scrollWheelZoom.enabled()) {
+    if (map.scrollWheelZoom.enabled()) {
       map.scrollWheelZoom.disable();
     }
   }
@@ -220,7 +235,7 @@
   }
 
   function onOverlayremove(e) {
-    activeLayers = activeLayers.filter(function(layer){
+    activeLayers = activeLayers.filter(function (layer) {
       return layer != titleToKey(e.name);
     });
     updateEmbedTextareaContentAndBrowserUrl();
@@ -257,18 +272,39 @@
 
   // Populate Fairphone Angels overlay
   fetchJSON('data/angels.json')
-    .then(function(json) {
-      // Add a marker per Heaven
-      json.heavens.forEach(function(heaven) {
-        if(heaven.exists && heaven.active) {
-          heaven.coordinates.forEach(function(lat_lng) {
-            var circle = L.circle(lat_lng, { radius: 30000, color: '#2ca7df', stroke:false, fillOpacity: 0.5 })
-            .bindPopup(
-              '<a href="mailto:' + heaven.location.toLowerCase() + '@' + COMMUNITY_DOMAIN + '">' + heaven.location + '<br>@' + COMMUNITY_DOMAIN + '<a>',
-            );
-            circle.addTo(overlaysData.angels.overlay);
-          })
-        }
+      .then(function (json) {
+        // Add a marker per Heaven
+        json.heavens.forEach(function (heaven) {
+          console.log(heaven.exists, heaven.active)
+          if (heaven.exists && heaven.active) {
+            heaven.coordinates.forEach(function (lat_lng) {
+              var circle = L.circle(lat_lng, {radius: 30000, color: '#2ca7df', stroke: false, fillOpacity: 0.5})
+                  .bindPopup(
+                      '<a href="mailto:' + heaven.location.toLowerCase() + '@' + COMMUNITY_DOMAIN + '">' + heaven.location + '<br>@' + COMMUNITY_DOMAIN + '<a>',
+                  );
+              circle.addTo(overlaysData.angels.overlay);
+            })
+          }
+        });
       });
-    });
+
+  // Populate Events & Meetups overlay (events fetched and parsed from https://events.fairphone.community/)
+  fetchJSON('data/events.json')
+      .then(function (events) {
+        // first sort events by date descending to ensure that two very close markers show the earlier
+        // event on top when the icons overlap
+        events.sort((a, b) => b.date < a.date ? -1 : b.date > a.date ? 1 : 0)
+        events.forEach(function (event) {
+          if (event.url && event.location && event.lat && event.lon && event.date >= new Date().toISOString().split('T')[0]) {
+            var popup = '<a href="' + event.url + '" target="_blank">' + event.location + '</a>' +
+                '<br><div class="shopinfo">Date: ' +
+                new Date(event.date).toLocaleDateString()
+            popup += '</div>';
+
+            var marker = L.marker([event.lat, event.lon], {icon: MARKERICONS.orange, riseOnHover: true})
+                .bindPopup(popup, {offset: new L.Point(0, -25)});
+            marker.addTo(overlaysData.events.overlay);
+          }
+        })
+      });
 }(this));
---
+++ scripts/fetch-events.py
@@ -0,0 +1,148 @@
+﻿import datetime
+import json
+import os.path
+import re
+
+import requests
+import sys
+
+pattern_table = re.compile("^\s*\|")
+pattern_date = re.compile("\d{1,2}([/\-.])\d{1,2}([/\-.])\d{4}") # DDMMYYY delimited by one of "/-."
+pattern_link_title = re.compile(".*\[[_*]*([\w\s]+)[_*]*].*", re.UNICODE) # ignore italics or bold markdown characters _ and *
+pattern_link = re.compile(".*\((https://forum.fairphone.com/t/.*)\)\s")
+pattern_coordinates = re.compile(".*<map.*?location=\"(\d+\.\d+)\s*?[/,]\s*?(\d+\.\d+)\".*?/>.*")
+pattern_map_title = re.compile(".*<map.*?title=\"([\w\s.-]+)\".*?/>.*", re.UNICODE)
+
+
+class Event:
+    def __init__(self, date, location, url, lat, lon: str):
+        self.date = date
+        self.location = location
+        self.url = url
+        self.lat = lat
+        self.lon = lon
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+def is_table_line(line):
+    return bool(pattern_table.match(line))
+
+
+def parse_date(datestr):
+    m = pattern_date.match(datestr)
+    if m is None:
+        return None
+    try:
+        parsed = datetime.datetime.strptime(datestr, '%d'+m.group(1)+'%m'+m.group(2)+'%Y')
+        if parsed >= datetime.datetime.now():
+            return parsed.date().strftime("%Y-%m-%d")
+        else:
+            return None # skip past events
+    except:
+        return None
+
+
+def parse_city(forumlink):
+    m = pattern_link_title.match(forumlink)
+    if m is None:
+        return None
+
+    return m.group(1)
+
+
+def parse_url(forumlink):
+    m = pattern_link.match(forumlink)
+    if m is None:
+        return None
+
+    return m.group(1)
+
+
+def parse_coordinates(locationcolumn):
+    m = pattern_coordinates.match(locationcolumn)
+    if m is None:
+        return None
+
+    return m.group(1) + "/" + m.group(2)
+
+
+def parse_location(locationcolumn):
+    m = pattern_map_title.match(locationcolumn)
+    if m is None:
+        return None
+
+    return m.group(1)
+
+
+def parse_event_from_table(line):
+    print("--- New line in event table ---")
+
+    columns = line.split("|")
+
+    date = parse_date(columns[1].replace(' ', ''))
+    print("Date: ", date)
+    if date is None:
+        return None
+
+    url = parse_url(columns[2])
+    print("URL : ", url)
+    if url is None:
+        return None
+
+    coordinates = parse_coordinates(columns[2])
+    print("Geo : ", coordinates)
+    if coordinates is None:
+        return None
+
+    city = parse_city(columns[2])
+    print("City: ", city)
+
+    location = parse_location(columns[2])
+    print("Titl: ", location)
+    if location is None:
+        location = city
+
+    print("Loc : ", location)
+    if location is None:
+        return None
+
+    lat_lon = coordinates.split("/")
+    return Event(date, location, url, lat_lon[0], lat_lon[1])
+
+
+response = requests.get('https://forum.fairphone.com/raw/87870/1')
+
+# if you want to test this script without fetching the real topic every time or need to manipulate the data before
+# parsing, save the topic raw data to file, comment out the get call above and use the lines below instead
+# file = open(os.path.dirname(os.path.abspath(__file__)) + "/../data/events_topic_raw.txt", 'r')
+# response = requests.Response()
+# response.status_code = requests.codes.ok
+# response._content = str.encode(file.read())
+
+if response.status_code == requests.codes.ok:
+    with open(os.path.dirname(os.path.abspath(__file__)) + "/../data/events.json", "w") as file:
+        event_table_state = 0  # when we hit the first table markup, we set to 1; when the table is finished we set to 2
+        events = []
+
+        for line in response.text.splitlines():
+            if is_table_line(line):
+                if event_table_state == 0:
+                    event_table_state = 1
+
+                event = parse_event_from_table(line)
+
+                if event is not None:
+                    events.append(event)
+            else:
+                if event_table_state == 1:
+                    event_table_state = 2
+
+            if event_table_state == 2:
+                break # only read the current events, don't read the past events table
+
+        output = "[\n" + ",\n".join(map(lambda x: x.toJson(), events)) + "\n]"
+        file.write(output)
+else:
+    sys.exit(1)
