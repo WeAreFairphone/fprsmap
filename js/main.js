b890797db2d5871ef229f5517dea3e8b2ff16c2d
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2026-09-15 19:30:48 +0200

    add API key when request map tiles
    Value is read from .env file locally and set from gh secrets in CI.

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@
   }
 
   function initMap(initialMapCenter, initialZoomLevel, defaultOverlays) {
-    var baseLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    var baseLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png?key=CARTO_API_KEY', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a> | &copy; <a href="https://github.com/WeAreFairphone/fprsmap" target="_blank">WeAreFairphone</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>)',
       maxZoom: 18,
     });
